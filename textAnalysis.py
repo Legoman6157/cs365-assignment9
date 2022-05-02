d5dae1a57f0a32acdee3c379e3dddd6d143525d7
Joshua Byers <joabyers@comcast.net>
2022-05-02 18:00:18 -0400

    Now collects all books where words occur

--- textAnalysis.py
+++ textAnalysis.py
@@ -9,49 +9,72 @@ Every apostrophe EXCEPT "A HERMIT'S WILD FRIENDS" needed to be changed
 
 from bs4 import BeautifulSoup
 import requests
+import re
+from collections import Counter
 
 if __name__ == '__main__':
+    o_file = open("outfile.txt", "w")
     url_file = open("sitesToScrap.csv", "r")
     url_file_line = url_file.readline()
+    MATCH_STRING = "[,\"\.&\|:@<>\(\)\*\$\?\!\\\/;=”“‘\[\]0-9—]"
+
+    book_urls_and_titles = []
+    book_word_lists = []
+    i = 1
+    word_instance_dict = {}
 
     while len(url_file_line) != 0:
+        temp_word_list = []
+
         url_string_and_title = url_file_line.split(',')
 
         url_string = url_string_and_title[0]
         title = url_string_and_title[1]
 
-        print('-'*81)
-
-        print("URL: " + url_string)
-        print("Title: " + title)
-
         title = title.strip('\n')
         if title != "A Hermit's Wild Friends":
             title = title.replace("'", "’")
+
+        book_urls_and_titles.append( (url_string, title) )
+
+        print('-'*81 + '\n')
+
+        print("URL: " + url_string + '\n')
+        print("Title: " + title + '\n')
 
         starting_string = "*** START OF THE PROJECT GUTENBERG EBOOK " + \
                             title.upper() + " ***"
         ending_string = "*** END OF THE PROJECT GUTENBERG EBOOK " + \
                             title.upper() + " ***"
 
-        print("Starting line: " + starting_string)
-        print("Ending line: " + ending_string)
-
         page = requests.get(url_string)
-        soup = BeautifulSoup(page.content, "html.parser")
+        body = BeautifulSoup(page.content, "html.parser").find("body")
 
-        starting_div = soup.find("div", string=starting_string)
-        ending_div = soup.find("div", string=ending_string)
+        reading_story = False
 
-        body = soup.find("body")
+        for tag in body:
+            if not reading_story:
+                if tag.text == starting_string:
+                    reading_story = True
+            else:
+                if tag.text == ending_string:
+                    break
 
-        starting_index = body.index(starting_div)
-        ending_index = body.index(ending_div)
+                string = tag.text
+                string = re.sub(MATCH_STRING, " ", string)
+                temp_word_list.extend(string.lower().split())
+                for word in temp_word_list:
+                    if word in word_instance_dict:
+                        if title not in word_instance_dict[word]:
+                            word_instance_dict[word].append(title)
+                    else:
+                        word_instance_dict[word] = [title]
 
-        print(starting_div)
+        o_file.write('-'*81 + '\n')
 
-        print('-'*81)
+        book_word_lists.append(temp_word_list)
 
         url_file_line = url_file.readline()
+        i = 0
 
     url_file.close()
